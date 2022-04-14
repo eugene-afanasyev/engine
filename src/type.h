f187b37bac4e86936dd77064057c9ed4c60dc63f
eugene-afanasyev <whoaeugene@gmail.com>
2022-04-14 23:21:41 +1000

    type aliases

---
+++ src/type.h
@@ -0,0 +1,14 @@
+#ifndef engine_type_h
+#define engine_type_h
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+
+namespace snowflake {
+
+using vec3 = glm::vec3;
+using mat4 = glm::mat4;
+
+}  // namespace snowflake
+
+#endif // engine_type_h
